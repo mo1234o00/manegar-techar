8a6cb368abe3d530c49a6c7febda9370e946e0e6
Salla Dev <rouallmostafa1234@gmail.com>
2026-09-03 20:39:25 +0300

    Restore dashboard as home page for teachers

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import { redirect } from 'next/navigation'
+import { DashboardClient } from '@/components/dashboard-client'
 
 export default function Home() {
-  redirect('/login')
+  return <DashboardClient />
 }
